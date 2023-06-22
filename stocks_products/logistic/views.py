a1f628d03a0919dec91b35a07d3329edbac41878
YlyaMart <yulya.martinova94@gmail.com>
2023-06-22 14:48:44 +0300

    Filter field added

--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -4,6 +4,8 @@ from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
 from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
+# from django_filters import rest_framework as filters
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
@@ -17,5 +19,6 @@ class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
-    filter_backends = [SearchFilter]
-    search_fields = ['product',]
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['products', ]
+    search_fields = ['products__title', 'products__description',]
